fa9652e531132d2c06ee963ae6fcda7575397dda
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-29 15:26:07 +0530

    ui: friend request page

--- src/components/Friends/FriendRequests.tsx
+++ src/components/Friends/FriendRequests.tsx
@@ -110,33 +110,39 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   });
 
   return (
-    <>
+    <div className="rounded-xl bg-white shadow-md p-3 w-1/2">
       {friendRequests.length === 0 ? (
         <p className="text-sm text-zinc-500">Nothing to show here...</p>
       ) : (
         friendRequests.map((request) => (
-          <div key={request.senderId} className="flex gap-4 items-center">
-            <UserPlus className="text-black" />
-            <p className="font-medium text-lg">{request.senderEmail}</p>
-
-            <button
-              aria-label="Accept friend request"
-              onClick={() => acceptFriend({ senderId: request.senderId })}
-              className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
-            >
-              <Check className="font-semibold text-white w-3/4 h-3/4" />
-            </button>
-            <button
-              aria-label="Decline friend request"
-              onClick={() => declineFriend({ senderId: request.senderId })}
-              className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
-            >
-              <X className="font-semibold text-white w-3/4 h-3/4" />
-            </button>
+          <div
+            key={request.senderId}
+            className="flex gap-4 items-center w-full justify-around"
+          >
+            <div className="flex items-center gap-x-3 w-full">
+              <UserPlus className="text-black" />
+              <p className="font-medium text-lg">{request.senderEmail}</p>
+            </div>
+            <div className="flex items-center justify-evenly w-2/3">
+              <button
+                aria-label="Accept friend request"
+                onClick={() => acceptFriend({ senderId: request.senderId })}
+                className="w-7 h-7 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
+              >
+                <Check className="font-semibold text-white w-3/4 h-3/4" />
+              </button>
+              <button
+                aria-label="Decline friend request"
+                onClick={() => declineFriend({ senderId: request.senderId })}
+                className="w-7 h-7 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
+              >
+                <X className="font-semibold text-white w-3/4 h-3/4" />
+              </button>
+            </div>
           </div>
         ))
       )}
-    </>
+    </div>
   );
 };
 
