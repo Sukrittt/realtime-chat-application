31600fb83f34569b8ad908830bf2621c00c5efab
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-29 21:40:48 +0530

    minor changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -60,11 +60,9 @@ const layout = async ({ children }: layoutProps) => {
         <nav className="flex flex-1 flex-col">
           <ul role="list" className="flex flex-1 flex-col gap-y-7">
             <li>
-              {/* {friends.length > 0 && ( */}
               <div className="text-xs font-semibold leading-6 text-gray-400">
                 Your chats
               </div>
-              {/* )} */}
               <SidebarChatLists friends={friends} sessionId={session.user.id} />
             </li>
             <li>
