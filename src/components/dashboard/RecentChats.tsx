32b8b537470e8657854b8c35762cf8bc9537179b
Sukrit Saha <sukritsaha27@gmail.com>
2023-06-29 21:38:53 +0530

    ui: display of recent chats

--- src/components/dashboard/RecentChats.tsx
+++ src/components/dashboard/RecentChats.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { chatHrefConstructor, formatName, trimMessage } from "@/lib/utils";
+import { formatDistanceToNow } from "date-fns";
 
 type ExtendedMessage = {
   lastMessage: Message | null;
@@ -18,54 +19,56 @@ const RecentChats: FC<RecentChatsProps> = ({
   friendsWithLastMessage,
   session,
 }) => {
+  const messageContent = (lastMessage: Message | null) => {
+    if (!lastMessage) return;
+
+    const providedTimeStamp = lastMessage.timestamp;
+
+    if (lastMessage.senderId === session.user.id) {
+      return `Sent ${formatDistanceToNow(providedTimeStamp, {
+        addSuffix: true,
+      })}`;
+    }
+
+    const trimmedText = trimMessage(lastMessage.text, 30);
+
+    return `${trimmedText} · ${formatDistanceToNow(providedTimeStamp, {
+      addSuffix: true,
+    })}`;
+  };
+
   return (
     <div className="col-span-2">
       <p className="text-zinc-700 mb-2 font-semibold text-sm">
         Your recent chats
       </p>
-      <div className="relative bg-white hover:bg-zinc-100 transition rounded-xl shadow-md p-3 flex flex-col gap-y-4 max-h-[400px] overflow-y-auto scrollbar-thumb-blue-lighter scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
+      <div className="relative bg-white rounded-xl shadow-md flex flex-col max-h-[400px] overflow-y-auto scrollbar-thumb-blue-lighter scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
         {friendsWithLastMessage.length === 0 ? (
           <p className="text-sm text-zinc-500">Nothing to show here...</p>
         ) : (
           friendsWithLastMessage.map((friend) => {
-            const formatedUserName = formatName(friend.name);
+            const messageDetails = messageContent(friend.lastMessage);
+            const filteredFriendsWithNoMessage = friendsWithLastMessage.filter(
+              (friend) => friend.lastMessage
+            );
+
+            if (!messageDetails && filteredFriendsWithNoMessage.length > 0)
+              return;
+
+            if (!messageDetails)
+              return (
+                <p className="text-sm text-zinc-500">
+                  Your recent chats will show up here...
+                </p>
+              );
 
             return (
-              <Link
+              <RecentChat
                 key={friend.id}
-                href={`/dashboard/chat/${chatHrefConstructor(
-                  session.user.id,
-                  friend.id
-                )}`}
-                className="relative sm:flex break-words"
-              >
-                <div className="mb-4 sm:mb-0 sm:mr-4">
-                  <div className="relative h-6 w-6">
-                    <Image
-                      referrerPolicy="no-referrer"
-                      src={friend.image || "/images/placeholder-user-3.png"}
-                      alt={`${friend.name}'s profile picture`}
-                      className="rounded-full"
-                      fill
-                    />
-                  </div>
-                </div>
-
-                <div className="break-words">
-                  <h4 className="text-zinc-800 font-medium">
-                    {formatedUserName}
-                  </h4>
-                  {friend.lastMessage && (
-                    <span className="mt-1 max-w-md break-words text-sm">
-                      <span className="text-zinc-400">
-                        {friend.lastMessage.senderId === session.user.id &&
-                          "You: "}
-                      </span>
-                      {trimMessage(friend.lastMessage.text, 40)}
-                    </span>
-                  )}
-                </div>
-              </Link>
+                friend={friend}
+                sessionId={session.user.id}
+                messageDetails={messageDetails}
+              />
             );
           })
         )}
@@ -75,3 +78,42 @@ const RecentChats: FC<RecentChatsProps> = ({
 };
 
 export default RecentChats;
+
+const RecentChat = ({
+  friend,
+  sessionId,
+  messageDetails,
+}: {
+  friend: ExtendedMessage;
+  sessionId: string;
+  messageDetails: string;
+}) => {
+  const formatedUserName = formatName(friend.name);
+
+  return (
+    <Link
+      key={friend.id}
+      href={`/dashboard/chat/${chatHrefConstructor(sessionId, friend.id)}`}
+      className="relative sm:flex break-words hover:bg-zinc-100 transition px-3 py-2"
+    >
+      <div className="mb-4 sm:mb-0 sm:mr-4">
+        <div className="relative h-6 w-6">
+          <Image
+            referrerPolicy="no-referrer"
+            src={friend.image || "/images/placeholder-user-3.png"}
+            alt={`${friend.name}'s profile picture`}
+            className="rounded-full"
+            fill
+          />
+        </div>
+      </div>
+
+      <div className="break-words">
+        <h4 className="text-zinc-800 font-medium">{formatedUserName}</h4>
+        <span className="mt-1 max-w-md break-words text-xs font-medium text-zinc-400">
+          {messageDetails}
+        </span>
+      </div>
+    </Link>
+  );
+};
